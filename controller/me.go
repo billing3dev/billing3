945057ca0608d3799b30cd22a1345392b801fce1
duoduo <sduoduo233@gmail.com>
2025-01-20 21:48:38 -0500

    return address of currently logged-in user

--- controller/me.go
+++ controller/me.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"billing3/controller/middlewares"
+	"fmt"
 	"net/http"
 )
 
@@ -9,9 +10,15 @@ import (
 func me(w http.ResponseWriter, r *http.Request) {
 	user := middlewares.MustGetUser(r)
 
+	address := fmt.Sprintf("%s, %s, %s, %s, %s", user.Address.String, user.City.String, user.State.String, user.Country.String, user.ZipCode.String)
+	if !user.Address.Valid && !user.City.Valid && !user.State.Valid && !user.Country.Valid && !user.ZipCode.Valid {
+		address = "unknown"
+	}
+
 	writeResp(w, http.StatusOK, D{
-		"email": user.Email,
-		"name":  user.Name,
-		"role":  user.Role,
+		"email":   user.Email,
+		"name":    user.Name,
+		"role":    user.Role,
+		"address": address,
 	})
 }
